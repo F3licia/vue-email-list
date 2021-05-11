3d703259cc15e0ec15414128d07914f5807d0f12
F3licia <romeofelicia@gmail.com>
2021-05-11 17:29:55 +0200

    completo con bonus

--- myScriptEmailList.js
+++ myScriptEmailList.js
@@ -2,22 +2,35 @@ const app = new Vue(
 
     {
         el: '#vueapp',
-            data: {
-                mailList:[],
-            
-            
-                },
-                mounted()  {
-                    for(let i =0; i < 10; i++) {
-                        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-                        .then((resp)=> {
-                            this.mailList.push(resp.data.response)
-                        });
-                }
-                    
+        data: {
+            mailList:[],
+            printList:[],
+            loading:false,
             },
-           
+            methods:{
+                catchData()  {
+                 this.loading = true;
+
+                    for(let i =0; i < 10; i++) {                       //ciclo for per ottenere 10 indirizzi
+                        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")          
+                        .then((resp)=> {
+                            setTimeout(()=> {                                    //set timeout per dilungare l'attesa (solo per mostrare messaggio)  
+
+                                this.mailList.push(resp.data.response);        //primo push a for completo
+
+                                    if(this.mailList.length === 10 ) {        
+                                        this.loading = false                 //secondo push e cambio stato 'loading'
+                                        this.printList = this.mailList       //la seconda lista sarà quella stampata
+                                        }
+                            }, 2000);
+                        });
+                    }
+                
+                },    
+            },
 
         }
 
 );
+
+

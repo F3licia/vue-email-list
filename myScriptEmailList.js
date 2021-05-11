6b3894ba45d5f0babc49ce7eb49e6e18d3a6fac0
F3licia <romeofelicia@gmail.com>
2021-05-11 16:07:35 +0200

    completato step1

---
+++ myScriptEmailList.js
@@ -0,0 +1,23 @@
+const app = new Vue(
+
+    {
+        el: '#vueapp',
+            data: {
+                mailList:[],
+            
+            
+                },
+                mounted()  {
+                    for(let i =0; i < 10; i++) {
+                        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                        .then((resp)=> {
+                            this.mailList.push(resp.data.response)
+                        });
+                }
+                    
+            },
+           
+
+        }
+
+);
